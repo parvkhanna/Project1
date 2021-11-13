f6a4737d9e52e4015f71e55880dccdfa31b8b92b
parvkhanna <parvkhanna27@gmail.com>
2021-11-13 15:24:18 +0530

    1

--- ID-24/src/test/java/com/vtiger/comcast/testscripts/CreateOrg.java
+++ ID-24/src/test/java/com/vtiger/comcast/testscripts/CreateOrg.java
@@ -4,6 +4,7 @@ package com.vtiger.comcast.testscripts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import com.vtiger.comcast.genericLibraries.DataBaseUtilities;
