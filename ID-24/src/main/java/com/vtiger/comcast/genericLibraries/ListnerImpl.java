c926b271168c9c108fb07d2440475bad10efca53
parvkhanna <parvkhanna27@gmail.com>
2021-11-13 14:40:51 +0530

    first

---
+++ ID-24/src/main/java/com/vtiger/comcast/genericLibraries/ListnerImpl.java
@@ -0,0 +1,87 @@
+package com.vtiger.comcast.genericLibraries;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListnerImpl implements ITestListener
+{
+
+	public void onTestStart(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) 
+	{
+		// TODO Auto-generated method stub
+String method = result.getMethod().getMethodName();
+		
+		Object obj = result.getInstance();
+		WebDriver driver = null;
+		 try 
+		 {
+			driver =(WebDriver)result.getTestClass().getRealClass().getSuperclass().getDeclaredField("driver").get(obj);
+		} catch (IllegalArgumentException e) 
+		 {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) 
+		 {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) 
+		 {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("./screenshot/"+method+".PNG");
+		try 
+		{
+	Files.copy(src, dest);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+				
+
+		
+	public void onTestSkipped(ITestResult result) 
+{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ ID-24/src/test/java/com/vtiger/comcast/testscripts/ListenerPractice.java
@@ -0,0 +1,18 @@
+package com.vtiger.comcast.testscripts;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.vtiger.comcast.genericLibraries.BaseAnnotationClass;
+@Listeners(com.vtiger.comcast.genericLibraries.ListnerImpl.class)
+public class ListenerPractice extends BaseAnnotationClass{
+
+	@Test
+	public void ListenerPractice()
+	{
+		System.out.println("test script failed");
+		Assert.fail();
+	}
+}
